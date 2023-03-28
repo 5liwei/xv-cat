909b3ff5a84d55b7932e1f454bff5dc6383d04da
李玮 <505805473@qq.com>
2023-03-28 23:05:29 +0800

    feat:new pro

---
+++ xvcat/miniprogram/custom-tab-bar/tab.js
@@ -0,0 +1,26 @@
+module.exports = {
+  genealogy: {
+    "pagePath": "pages/genealogy/genealogy",
+    "iconPath": "/pages/public/images/tabs/default/cat.png",
+    "selectedIconPath": "/pages/public/images/tabs/active/cat.png",
+    "text": "西猫"
+  },
+  news: {
+    "pagePath": "pages/news/news",
+    "iconPath": "/pages/public/images/tabs/default/sci.png",
+    "selectedIconPath": "/pages/public/images/tabs/active/sci.png",
+    "text": "猫抓板"
+  },
+  leaderboard: {
+    "pagePath": "pages/leaderboard/leaderboard",
+    "iconPath": "/pages/public/images/tabs/default/org.png",
+    "selectedIconPath": "/pages/public/images/tabs/active/org.png",
+    "text": "精选"
+  },
+  info: {
+    "pagePath": "pages/info/info",
+    "iconPath": "/pages/public/images/tabs/default/info.png",
+    "selectedIconPath": "/pages/public/images/tabs/active/info.png",
+    "text": "关于"
+  }
+}
